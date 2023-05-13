84bc6f8e4f153190d0fd2802e26f5dd047acb8b6
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-05-13 15:56:14 -0500

    Revert "added basic setup"
    This reverts commit 3f88fb9a475f2c3f2a4b6f3772e2b63d47fd7b74.

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,23 +1,16 @@
 const eventListeners = (() => {
-  const projectForm = () => {
-    const addProjectBtn = document.getElementById("add-project-button");
-    const projectFormElement = document.getElementById("project-form");
-    addProjectBtn.addEventListener("click", () => {
-      projectFormElement.style.visibility = "visible";
-      return projectFormListener.bind(projectForm);
-    });
-    const projectFormListener = () => {
-      const getItems = () => {
-        const projectName = document.getElementById("project-name-field");
-        this.title = projectName.value;
-      };
-      const submitForm = document.getElementById("project-submit-button");
-      submitForm.addEventListener("click", getItems.bind(projectFormListener));
-      return this.title;
-    };
+  const submitFormProject = () => {
+    const projectName = document.getElementById("project-name-field");
   };
+
+  const addProjectBtn = document.getElementById("add-project-button");
+  const projectFrom = document.getElementById("project-form");
+  addProjectBtn.addEventListener("click", () => {
+    projectFrom.style.visibility = "visible";
+    createProject();
+  });
   return {
-    projectForm,
+    submitFormProject,
   };
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,46 +1,4 @@
 import DOMmethods from "./DOMmethods";
-import Project from "./project";
-import Container from "./container";
+import eventListeners from "./eventListeners";
 
-const eventListeners = (() => {
-  const allContainer = new Container("Inbox");
-  DOMmethods.displayTitle(allContainer, "hidden");
-
-  const getItems = () => {
-    const projectName = document.getElementById("project-name-field");
-    const title = projectName.value;
-    const project = new Project(title);
-    DOMmethods.displayTitle(project, "visible");
-    allContainer.add(project);
-    DOMmethods.displayProjects(allContainer.projectArray);
-    selectProject(allContainer);
-  };
-  const addProjectBtn = document.getElementById("add-project-button");
-  const projectFormElement = document.getElementById("project-form");
-  addProjectBtn.addEventListener("click", () => {
-    projectFormElement.style.visibility = "visible";
-    const submitForm = document.getElementById("project-submit-button");
-    submitForm.addEventListener("click", getItems);
-    const cancelForm = document.getElementById("project-cancel-button");
-    cancelForm.addEventListener("click", () => {
-      projectFormElement.style.visibility = "hidden";
-      projectFormElement.reset();
-    });
-  });
-
-  const inboxBtn = document.getElementById("inbox");
-  inboxBtn.addEventListener("click", () => {
-    DOMmethods.displayTitle(allContainer, "hidden");
-  });
-
-  const selectProject = (container) => {
-    const projectBtns = document.querySelectorAll(".project-title");
-    projectBtns.forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        const index = e.target.id.slice(-1);
-        const project = container.projectArray[index];
-        DOMmethods.displayTitle(project, "visible");
-      });
-    });
-  };
-})();
+DOMmethods.test();
